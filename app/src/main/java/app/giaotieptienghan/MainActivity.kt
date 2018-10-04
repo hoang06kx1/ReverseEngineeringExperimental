acdbbbfc95fc1e5985bd853ef5c3096e3a33ef11
hoangnn <hoangnn@justgola.com>
2018-10-04 23:30:09 +0700

    Log

--- app/src/main/java/app/giaotieptienghan/MainActivity.kt
+++ app/src/main/java/app/giaotieptienghan/MainActivity.kt
@@ -13,7 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("Key", ObfuscatedString(longArrayOf(54233370504433150L, 4898902494104535733L, -5807597149084756791L, 8156855886985688596L)).toString())
         this.f1950d = Decryption(ObfuscatedString(longArrayOf(54233370504433150L, 4898902494104535733L, -5807597149084756791L, 8156855886985688596L)).toString())
         Log.d("Decryption", this.f1950d!!.decrypt("jxZnfVBpuzX27vCAJ9zn9l83VBGeXPFa"))
         Log.d("Decryption", this.f1950d!!.decrypt("HIWTkTesIqjUh1AyDKl+ReVIBqovx1K6"))
